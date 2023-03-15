3c595de3cd205f5805e303d8255ab136602b9d35
yoktak <yokotaku327@gmail.com>
2023-03-15 09:32:36 +0900

    [add]migration作成

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('profile');
+            $table->string('icon')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreignId('user_id')->constrained('items')->onDelete('coscade');
             $table->rememberToken();
             $table->timestamps();
         });
